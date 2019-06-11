b275675d3784095d7ad53bb44c94f731dabc5ae3
dillon <dpett122@gmail.com>
2019-06-11 19:21:10 -0400

    restructure folders

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './root/App';
-import * as serviceWorker from './serviceWorker';
+import './main/index.css';
+import App from "./renderer";
+import * as serviceWorker from './main/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
