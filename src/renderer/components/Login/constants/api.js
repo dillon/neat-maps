af9b33d5b39da8b81763e1e5e25a4496c2e7f3fb
dillon <dpett122@gmail.com>
2019-06-15 22:45:24 -0400

    chore(app): make app deployable to gh-pages

--- src/renderer/components/Login/constants/api.js
+++ src/renderer/components/Login/constants/api.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const url = 'http://neat-mvp-api.herokuapp.com/v1/auth'
+export const url = 'https://neat-mvp-api.herokuapp.com/v1/auth'
